c77cc50de781ff18e74d322690eba1ab87a6425f
Jester <dagotv0@gmail.com>
2025-09-23 16:10:31 +0500

    Delete Topic3 directory

--- Topic3/LabTask2.py
+++
@@ -1,7 +0,0 @@
-number = int(input('Введите число: '))
-if number < 0:
-    print('Число меньше 0.')
-elif 0 < number < 10:
-    print('Число больше 0, но меньше 10.')
-else:
-    print('Число больше 10.')
--- Topic3/LabTask3.py
+++
@@ -1,6 +0,0 @@
-numbers = [1, 2, 4, 8, 16]
-number = int(input('Введите число: '))
-if number in numbers:
-    print('Число есть в массиве.')
-else:
-    print('Нет числа.')
