6ea0294f2fa0720de1fe79738cbec8a125eb29f2
Yukio <dagotv0@gmail.com>
2025-10-11 20:56:06 +0500

    1

---
+++ LabTask/LabTask5.py
@@ -0,0 +1,5 @@
+def useless(lst):
+    return max(lst)/len(lst)
+print(useless([3, 5, 3, 7, 33]))
+print(useless([-12.5, 54, 77.3, 0, -36, 98.2, -63, 21.7]))
+print(useless([-25.8, 86, 12.5, -56, -73.2, 0, 43, -91.5]))
---
+++ LabTask/LabTask6.py
@@ -0,0 +1,7 @@
+supeheroes = ['superman', 'spiderman', 'batman']
+
+nikolay, vasiliy, ivan = supeheroes
+
+print('Николай - ', nikolay)
+print('Василий - ', vasiliy)
+print('Иван - ', ivan)
